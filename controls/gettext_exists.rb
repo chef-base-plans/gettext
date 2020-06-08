6c9095fb160b56f0214bbdd29db0f8b73b707460
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 17:14:51 +0100

    Add badge, test descriptions, and calculate the pkg_version
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/gettext_exists.rb
+++ controls/gettext_exists.rb
@@ -1,17 +1,23 @@
 title 'Tests to confirm gettext exists'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'gettext')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-gettext_relative_path = input('command_path', value: '/bin/gettext')
-gettext_installation_directory = command("hab pkg path #{plan_ident}")
-gettext_full_path = gettext_installation_directory.stdout.strip + "#{ gettext_relative_path}"
- 
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+command_relative_path = input('command_relative_path', value: '/bin/gettext')
+command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+
 control 'core-plans-gettext-exists' do
   impact 1.0
   title 'Ensure gettext exists'
   desc '
-  '
-   describe file(gettext_full_path) do
+  Verify gettext by ensuring /bin/gettext exists'
+  
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  describe file(command_full_path) do
     it { should exist }
   end
 end
--- controls/gettext_works.rb
+++ controls/gettext_works.rb
@@ -1,24 +1,28 @@
 title 'Tests to confirm gettext works as expected'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'gettext')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
 
 control 'core-plans-gettext-works' do
   impact 1.0
   title 'Ensure gettext works as expected'
   desc '
+  Verify gettext by ensuring (1) its installation directory exists and (2) that
+  it returns the expected version
   '
-  gettext_path = command("hab pkg path #{plan_ident}")
-  describe gettext_path do
+  
+  describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
   
-  gettext_pkg_ident = ((gettext_path.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  describe command("DEBUG=true; hab pkg exec #{ gettext_pkg_ident} gettext --version") do
+  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} gettext --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /gettext \(GNU gettext-runtime\) 0.20.1/ }
+    its('stdout') { should match /gettext \(GNU gettext-runtime\) #{plan_pkg_version}/ }
     its('stderr') { should be_empty }
   end
 end
